aff43072bf4f32dbc96ea748b85686664de026bd
Lostjerome <naronkrach@gmail.com>
2024-10-23 12:49:02 +0700

    Setup i18n utils and ui

---
+++ src/i18n/ui.ts
@@ -0,0 +1,18 @@
+export const languages = {
+    en: "English",
+    th: "ไทย",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+    en: {
+        "nav.home": "Home",
+        "nav.about": "About",
+        "nav.twitter": "Twitter",
+    },
+    th: {
+        "nav.home": "หน้าหลัก",
+        "nav.about": "เกี่ยวกับเรา",
+    },
+} as const;
