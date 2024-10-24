6a030b4a4c0b1b93eebad8489f2bf6d09ba61e57
celesca <folk.sawit@gmail.com>
2024-10-25 01:31:04 +0700

    fix: category styling

--- src/components/BlogItem.tsx
+++ src/components/BlogItem.tsx
@@ -20,11 +20,16 @@ const BlogItem: React.FC<Props> = ({ thumbnail, categories, title, content, auth
         <img src={thumbnail.src} alt={thumbnail.alt} className="w-full h-full object-cover" />
         <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
-      <div className="flex justify-between items-center mt-2">
-        <span className="bg-[#46000C] text-red-600 px-2 py-1 rounded-full text-xs">{categories.join(', ')}</span>
+      <div className="flex items-center mt-2">
+        {
+            categories.map((category, index) => (
+                <span key={index} className="bg-[#46000C] text-red-600 rounded-full text-md px-3 py-1 mr-2">{category.toUpperCase()}</span>
+            ))
+        }
+        
       </div>
-      <h2 className="text-xl font-bold">{title}</h2>
-      <p className="text-gray-400">{content}</p>
+      <h2 className="text-2xl font-bold">{title}</h2>
+      <p className="text-gray-400 text-md">{content}</p>
       <div className="flex items-center space-x-2">
           <img src={authorAvatar} className="w-8 h-8 object-cover rounded-full" alt={`${author}'s avatar`} />
           <div className="text-sm">
