080bf2e6f191b2c90ea30ab6923140b6f6012780
Lostjerome <naronkrach@gmail.com>
2024-10-24 17:12:01 +0700

    Add responsive

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -9,24 +9,25 @@ interface Props {
 
 const Banner: React.FC<Props> = ({ tag, title, description, cover }) => {
     return (
-        <div className="p-2">
-            <div className="relative py-36 rounded-3xl overflow-clip">
+        <div className="p-1 md:p-2">
+            <div className="relative py-40 rounded-2xl md:rounded-3xl overflow-clip">
                 <img
                     src={cover.src}
                     className="absolute top-0 left-0 w-full h-full object-cover"
                 />
                 <div className="absolute bg-black opacity-80 w-full h-full top-0"></div>
                 <div className="absolute bg-primary-500 w-full h-full top-0 mix-blend-overlay"></div>
-                <div className="relative max-w-[48rem] text-center m-auto space-y-6 z-10">
+
+                <div className="relative max-w-[48rem] text-center m-auto space-y-4 md:space-y-6 z-10">
                     <div className="space-y-3">
                         <small className="text-primary-500">
                             <b>{tag}</b>
                         </small>
-                        <h1 className="font-bold text-4xl text-gray-200">
+                        <h1 className="font-bold text-3xl md:text-4xl text-gray-200">
                             {title}
                         </h1>
                     </div>
-                    <p className="text-lg">{description}</p>
+                    <p className="text-sm md:text-lg">{description}</p>
                 </div>
             </div>
         </div>
