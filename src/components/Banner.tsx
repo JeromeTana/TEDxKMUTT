61be0e1c1a115e01dbadc96bb6ac22e35f5fc4ff
Lostjerome <naronkrach@gmail.com>
2024-10-24 16:59:00 +0700

    Add React banner

---
+++ src/components/Banner.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+interface Props {
+    tag: string;
+    title: string;
+    description: string;
+    cover: ImageMetadata;
+}
+
+const Banner: React.FC<Props> = ({ tag, title, description, cover }) => {
+    return (
+        <div className="p-2">
+            <div className="relative py-36 rounded-3xl overflow-clip">
+                <img
+                    src={cover.src}
+                    className="absolute top-0 left-0 w-full h-full object-cover"
+                />
+                <div className="absolute bg-black opacity-80 w-full h-full top-0"></div>
+                <div className="absolute bg-primary-500 w-full h-full top-0 mix-blend-overlay"></div>
+                <div className="relative max-w-[48rem] text-center m-auto space-y-6 z-10">
+                    <div className="space-y-3">
+                        <small className="text-primary-500">
+                            <b>{tag}</b>
+                        </small>
+                        <h1 className="font-bold text-4xl text-gray-200">
+                            {title}
+                        </h1>
+                    </div>
+                    <p className="text-lg">{description}</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
