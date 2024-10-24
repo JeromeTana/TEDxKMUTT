b8b8ac6e2ec584b2bda310d990cd47d1502a6c23
celesca <folk.sawit@gmail.com>
2024-10-25 01:42:35 +0700

    adjusted the image height

--- src/components/BlogItem.tsx
+++ src/components/BlogItem.tsx
@@ -17,21 +17,22 @@ const BlogItem: React.FC<Props> = ({ thumbnail, categories, title, content, auth
   return (
     <div className="text-white p-4 rounded-lg space-y-4">
       <div className="relative rounded-2xl overflow-hidden">
-        <img src={thumbnail.src} alt={thumbnail.alt} className="w-full h-full object-cover" />
+        <img src={thumbnail.src} alt={thumbnail.alt} className="h-52 w-auto object-cover" />
         <div className="absolute inset-0 bg-black opacity-50"></div>
       </div>
-      <div className="flex items-center mt-2">
+      <div className="flex items-center">
+        
         {
             categories.map((category, index) => (
-                <span key={index} className="bg-[#46000C] text-red-600 rounded-full text-md px-3 py-1 mr-2">{category.toUpperCase()}</span>
+                <span key={index} className="bg-[#46000C] mt-6 mb-2 text-red-600 rounded-full text-sm px-3 py-1 mr-2">{category.toUpperCase()}</span>
             ))
         }
         
       </div>
       <h2 className="text-2xl font-bold">{title}</h2>
       <p className="text-gray-400 text-md">{content}</p>
-      <div className="flex items-center space-x-2">
-          <img src={authorAvatar} className="w-8 h-8 object-cover rounded-full" alt={`${author}'s avatar`} />
+      <div className="flex items-center space-x-1 space-y-4">
+          <img src={authorAvatar} className="w-8 h-8 object-cover rounded-full mr-2" alt={`${author}'s avatar`} />
           <div className="text-sm">
             <p>{author}</p>
             <p className="text-xs text-gray-400">{pubDate}</p>
