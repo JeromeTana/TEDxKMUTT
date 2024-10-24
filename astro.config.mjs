198fd80824cb90832c8064cb2c0c5e1fbd7ebaf5
Lostjerome <naronkrach@gmail.com>
2024-10-24 13:20:08 +0700

    Integrate react

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,11 @@ import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import netlify from "@astrojs/netlify";
 
+import react from "@astrojs/react";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [tailwind(), icon()],
+    integrations: [tailwind(), icon(), react()],
     output: "server",
     adapter: netlify(),
 });
