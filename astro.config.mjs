172e10a037c16bb040d7229e24ea5a39a2eb2194
Lostjerome <naronkrach@gmail.com>
2024-11-28 09:28:40 +0700

    Disable image service that uncompatible with Cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig, passthroughImageService } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
@@ -11,4 +11,7 @@ export default defineConfig({
     integrations: [tailwind(), icon(), react()],
     output: "server",
     adapter: cloudflare(),
+    image: {
+        service: passthroughImageService(),
+    },
 });
